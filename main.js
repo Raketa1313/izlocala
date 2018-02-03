95a3edf0ba1351bddd40e5a1883d14dfc596f6f5
Andrey <lomonosova13011976@gmail.com>
2018-02-03 11:28:20 +0200

    старт меню

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+
+    $('.touch-menu').click(function(event) {
+        $('.box_menu, .touch-menu').toggleClass('active');
+    });
+
+    // $('.box_menu li').hover(
+    //     function() {
+    //         $(this).css('width', '30%');
+    //     }, function() {
+    //         $(this).css('width', '20%');
+    //     }
+    // );
+
+});
